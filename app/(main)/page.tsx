ea470dd5b988ea9aa8ac7d9fd0a01b0807c1f00b
Haleychoioi <157025597+Haleychoioi@users.noreply.github.com>
2025-09-27 17:45:29 +0900

    재사용 버튼 구현

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,10 @@
+import Button from "@/components/common/Button";
+
 export default function HomePage() {
   return (
-    <div className="h-screen"><h1 className="text-4xl font-bold">HomePage</h1></div>
+    <div className="h-screen"><h1 className="text-4xl font-bold">HomePage</h1>
+    <Button>Click me</Button>
+    <Button color="warning">정지</Button>
+    </div>
   );
 }
